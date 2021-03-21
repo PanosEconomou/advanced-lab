3c6b9c96d36e5035d06b97e29ce9338a6ac38cd7
Panos Oikonomou <panos.economou.v@gmail.com>
2021-03-21 11:40:24 +0400

    Fixed Realtime Plotting in Proceedure 1

--- 2.Faraday-Rotation/2.Code/Faraday_Controller/proceedure1.py
+++ 2.Faraday-Rotation/2.Code/Faraday_Controller/proceedure1.py
@@ -2,6 +2,8 @@ from oscillovisa import *
 from clilib import *
 from csvlib import *
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 import time
 from tqdm import tqdm
 
@@ -21,12 +23,29 @@ oscilloscope = get_intstrunment(VERBOSE=VERBOSE)
 angle = np.linspace(min_angle,max_angle,Nangles,dtype=int)
 V = []
 V_std = []
+a_data = []
 
 # Set the speed
 set_speed(MOTOR_SPEED,arduino,VERBOSE=VERBOSE)
 
+
+# Relevant Realtime Plotting Variables
+fig = plt.figure(figsize = (10,7),dpi=100)
+ax = fig.add_subplot()
+
+ln, = plt.plot([], [],'+',c='darkblue',label='Raw Data')
+
+# Initialises the plot
+def init():
+    ax.set_xlabel(r'Angle $\Delta \phi$ [degrees]')
+    ax.set_ylabel(r'Voltage V [V]')
+    ax.set_title('Voltage VS Angle')
+    ax.legend(frameon=False)
+    return ln,
+
+# The actual Experimental Proceedure
 # For all angles
-for a in tqdm(angle):
+def get_measurement(a):
     # Set the polariser
     move_to_angle(a,arduino,VERBOSE=VERBOSE)
 
@@ -42,7 +61,22 @@ for a in tqdm(angle):
     get_data_csv(oscilloscope,channels=[1,2,3],directory=directory,filename='Angle-'+str(a)+'.csv')
     plot_oscilloscope(oscilloscope,channels=[1,2,3],VERBOSE=VERBOSE,PLOT=False,save_image=True,filename=directory+'Angle-'+str(a))
 
-# Output the values
+    # Relevant Plotting stuff
+    a_data.append(a)
+    dV = max(V)-min(V)
+    da = max(a_data)-min(a_data)
+    if dV!=0 and da!=0:
+        ax.set_xlim(min(a_data)-0.1*da, max(a_data)+0.1*da)
+        ax.set_ylim(min(V)-0.1*dV, max(V)+0.1*dV)
+
+    ln.set_data(a_data, V)
+    return ln,
+
+# Perform the Run
+ani = FuncAnimation(fig, get_measurement, frames=tqdm(angle),interval=10,init_func=init, blit=False)
+plt.show()
+
+# Output the values to CSV 
 V = np.array(V)
 V_std = np.array(V_std)
 create_csv(name='Voltage-Amplitude.csv',directory=directory,titles=['Angle','Voltage','Voltage_std'],data=[angle,V,V_std])
--- 2.Faraday-Rotation/2.Code/Faraday_Controller/test.py
+++ 2.Faraday-Rotation/2.Code/Faraday_Controller/test.py
@@ -2,20 +2,58 @@ from oscillovisa import *
 from clilib import *
 import serial 
 import time
+import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
 
-arduino = serial.Serial(port='/dev/cu.usbmodem14101',baudrate=115200,timeout=0.1)
-# oscilloscope = get_intstrunment(VERBOSE=VERBOSE)
-# time.sleep(2)
+# arduino = serial.Serial(port='/dev/cu.usbmodem14101',baudrate=115200,timeout=0.1)
+# # oscilloscope = get_intstrunment(VERBOSE=VERBOSE)
+# # time.sleep(2)
 
-# while True:
-#     cmd = 100128
-#     send_command(cmd,arduino,VERBOSE=False)
-#     read_monitor(arduino)
-#     # print(arduino.readline())
-#     time.sleep(0.01)
+# # while True:
+# #     cmd = 100128
+# #     send_command(cmd,arduino,VERBOSE=False)
+# #     read_monitor(arduino)
+# #     # print(arduino.readline())
+# #     time.sleep(0.01)
 
-# arduino.close()
+# # arduino.close()
 
-inst = get_intstrunment(VERBOSE=True)
-plot_oscilloscope(inst,dpi=70,VERBOSE=True)
-# get_data_csv(inst)
+# inst = get_intstrunment(VERBOSE=True)
+# plot_oscilloscope(inst,dpi=70,VERBOSE=True)
+# # get_data_csv(inst)
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+fig = plt.figure(figsize = (10,7),dpi=100)
+ax = fig.add_subplot()
+ax.set_xlabel(r'Angle $\Delta \phi$ [degrees]')
+ax.set_ylabel(r'Voltage V [V]')
+ax.set_title('Voltage VS Angle')
+
+xdata, ydata, std = [], [], []
+ln, = plt.plot([], [],'+',c='darkblue',label='Raw Data')
+
+def init():
+    ax.legend(frameon=False)
+    return ln,
+
+def update(frame):
+    xdata.append(frame)
+    ydata.append(np.sin(frame))
+    std.append(np.random.random()*0.2)
+
+    dx = max(xdata)-min(xdata)
+    dy = max(ydata)-min(ydata)
+    if dx!=0 and dy!=0:
+        ax.set_xlim(min(xdata)-0.1*dx, max(xdata)+0.1*dx)
+        ax.set_ylim(min(ydata)-0.1*dy, max(ydata)+0.1*dy)
+
+    ln.set_data(xdata, ydata)
+    return ln,
+
+ani = FuncAnimation(fig, update, frames=tqdm(np.linspace(0, 2*np.pi, 50)),interval=10,init_func=init, blit=False)
+plt.show()
